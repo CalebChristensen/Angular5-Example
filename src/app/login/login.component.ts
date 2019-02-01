59239a61aca6dc974946f40f7882a203bc75d9c1
Caleb Christensen <Caleb.Christ@gmail.com>
2019-02-01 14:58:53 -0500

    login.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
   }
 
+  login() {
+    if (this.email == 'Caleb@aol.com' && this.password == 'Caleb') {
+      this.router.navigate(['user']);
+    } else {
+      alert("Invalid credentials.")
+    }
+  }
 }
